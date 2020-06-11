a039b5749ce27b736bbfc1454f79fbfa9930c0fe
Nicholas Henry <nicholas.henry@gmail.com>
2020-06-11 11:48:53 -0400

    Generate application

---
+++ lib/world_temp/application.ex
@@ -0,0 +1,19 @@
+defmodule WorldTemp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: WorldTemp.Worker.start_link(arg)
+      # {WorldTemp.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: WorldTemp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
